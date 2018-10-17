532efe7491e90661f24bc5e8a6c5873443494c97
Eric Fritz <eric.fritz@mitel.com>
2018-10-17 12:27:48 -0500

    Update docs for directory sourcer.

--- directory_sourcer.go
+++ directory_sourcer.go
@@ -9,7 +9,10 @@ type directorySourcer struct {
 	err error
 }
 
-// TODO
+// NewDirectorySourcer creates a sourcer that reads files from a directory. For
+// details on parsing format, refer to NewFileParser. Each file in a directory
+// is read in alphabetical order. The directory is assumed to have only files
+// and each file must be parseable by the given FileParser.
 func NewDirectorySourcer(dirname string, parser FileParser) Sourcer {
 	entries, err := ioutil.ReadDir(dirname)
 	if err != nil {
