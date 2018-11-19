88abf19201ea86556bbe8637f87f500aaa5a5d6e
Eric Fritz <eric.fritz@mitel.com>
2018-11-19 11:47:47 -0600

    Export logging options.

--- logging.go
+++ logging.go
@@ -4,4 +4,7 @@ import "github.com/efritz/zubrin/logging"
 
 type Logger = logging.Logger
 
-var NewLoggingConfig = logging.NewLoggingConfig
+var (
+	NewLoggingConfig = logging.NewLoggingConfig
+	WithMaskedKeys   = logging.WithMaskedKeys
+)
