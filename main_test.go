6ba9d8955e0982f57a34bc96149efda643779dbb
Eric Fritz <eric@eric-fritz.com>
2019-09-05 08:45:24 -0500

    Create internal fs mock.

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package config
 //go:generate go-mockgen -f github.com/go-nacelle/config -i Config -o config_mock_test.go
 //go:generate go-mockgen -f github.com/go-nacelle/config -i Logger -o logger_mock_test.go
 //go:generate go-mockgen -f github.com/go-nacelle/config -i Sourcer -o sourcer_mock_test.go
+//go:generate go-mockgen -f github.com/go-nacelle/config -i FileSystem -o fs_mock_test.go
 
 import (
 	"reflect"
