c2997d643ea14c8de446e19e756b7dc31def9162
Eric Fritz <eric@eric-fritz.com>
2021-01-03 16:57:05 -0600

    Update mocks.

--- gen.go
+++ gen.go
@@ -1,6 +1,5 @@
 package config
 
-//go:generate go-mockgen -f github.com/go-nacelle/config -i Config -o mock_config_test.go
 //go:generate go-mockgen -f github.com/go-nacelle/config -i Logger -o mock_logger_test.go
 //go:generate go-mockgen -f github.com/go-nacelle/config -i Sourcer -o mock_sourcer_test.go
 //go:generate go-mockgen -f github.com/go-nacelle/config -i FileSystem -o mock_fs_test.go
