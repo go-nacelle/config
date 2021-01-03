0171e09a12e31743c9f78934ee8a67ba69f9a264
Eric Fritz <eric@eric-fritz.com>
2021-01-03 12:11:53 -0600

    Update mock interface filenames.

--- gen.go
+++ gen.go
@@ -1,6 +1,6 @@
 package config
 
-//go:generate go-mockgen -f github.com/go-nacelle/config -i Config -o config_mock_test.go
-//go:generate go-mockgen -f github.com/go-nacelle/config -i Logger -o logger_mock_test.go
-//go:generate go-mockgen -f github.com/go-nacelle/config -i Sourcer -o sourcer_mock_test.go
-//go:generate go-mockgen -f github.com/go-nacelle/config -i FileSystem -o fs_mock_test.go
+//go:generate go-mockgen -f github.com/go-nacelle/config -i Config -o mock_config_test.go
+//go:generate go-mockgen -f github.com/go-nacelle/config -i Logger -o mock_logger_test.go
+//go:generate go-mockgen -f github.com/go-nacelle/config -i Sourcer -o mock_sourcer_test.go
+//go:generate go-mockgen -f github.com/go-nacelle/config -i FileSystem -o mock_fs_test.go
